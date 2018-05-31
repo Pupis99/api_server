3cc2099b3e59aaaf9be3bfe43ca39a052d34ae85
Pupis99 <=>
2018-05-31 00:29:52 -0700

    Segunda subida

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+
 
 const Persona = require('./routes/persona');
 const Secret = require('./routes/secret');
@@ -14,8 +16,16 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
+
+//app.use(express.static(path.join(__dirname, 'logacode')));
+app.use('/', express.static('logacode', {redirect: false}));
 app.use('/api/persona', Persona);
 app.use('/api/secret', Secret);
 app.use('/api/login', Login);
+
+/*app.get('*', function(req, res, next)
+{
+    res.sendFile(path.resolve('logacode/index.html'));
+});*/
 
 module.exports = app;
